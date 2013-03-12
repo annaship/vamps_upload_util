8bcad9d57860b63dae2f727677412e1098dddac5
annaship <tetya-loshad@yandex.ru>
2013-03-12 12:20:05 -0400

    fix serever names

--- compare_interm_w_current.py
+++ compare_interm_w_current.py
@@ -3,12 +3,12 @@ import sys
 import collections
 import shared #use shared to call connection from outside of the module
 
-# shared.my_conn = vamps_upload_util_class.MyConnection('vampsdev', 'test')
+# shared.my_conn = vamps_upload_util_class.MyConnection('bpcweb7.bpcservers.private', 'test')
 # shared.my_conn = vamps_upload_util_class.MyConnection('newbpcdb2', 'env454')
-# shared.my_conn = vamps_upload_util_class.MyConnection('BPCWeb8.mbl.edu', 'vamps')
-# shared.my_conn = vamps_upload_util_class.MyConnection('vampsdev', 'vamps2')
+# shared.my_conn = vamps_upload_util_class.MyConnection('vampsdb', 'vamps')
+# shared.my_conn = vamps_upload_util_class.MyConnection('bpcweb7.bpcservers.private', 'vamps2')
 
 if __name__ == '__main__':
-  shared.my_conn = vamps_upload_util_class.MyConnection('vampsdb', 'vamps')
+  shared.my_conn = vamps_upload_util_class.MyConnection('bpcweb7.bpcservers.private', 'vamps2')
   vamps_upload_util_class.SqlUtil().compare_interm_w_current()
 
--- compare_res_w_previous.py
+++ compare_res_w_previous.py
@@ -5,12 +5,12 @@ import shared #use shared to call connection from outside of the module
 
 # shared.my_conn = vamps_upload_util_class.MyConnection(server_name = 'bpcdb2')
 # shared.my_conn = vamps_upload_util_class.MyConnection(server_name = 'vampsdb')
-# shared.my_conn = vamps_upload_util_class.MyConnection('vampsdev', 'test')
+# shared.my_conn = vamps_upload_util_class.MyConnection('bpcweb7.bpcservers.private', 'test')
 # shared.my_conn = vamps_upload_util_class.MyConnection('newbpcdb2', 'env454')
-# shared.my_conn = vamps_upload_util_class.MyConnection('vampsdb.mbl.edu', 'vamps')
-# shared.my_conn = vamps_upload_util_class.MyConnection('vampsdev', 'vamps2')
+# shared.my_conn = vamps_upload_util_class.MyConnection('vampsdb', 'vamps')
+# shared.my_conn = vamps_upload_util_class.MyConnection('bpcweb7.bpcservers.private', 'vamps2')
 
 if __name__ == '__main__':
-  shared.my_conn = vamps_upload_util_class.MyConnection('vampsdb.mbl.edu', 'vamps')
+  shared.my_conn = vamps_upload_util_class.MyConnection('bpcweb7.bpcservers.private', 'vamps2')
   vamps_upload_util_class.SqlUtil().compare_res_w_previous(table_list = ["vamps_data_cube", "vamps_export", "vamps_junk_data_cube", "vamps_projects_datasets", "vamps_sequences", "vamps_taxonomy"])
 
