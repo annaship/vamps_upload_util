31d2adf2e8784e9ac89b31359faf42a8b9e3d5bd
annaship <tetya-loshad@yandex.ru>
2016-12-19 16:54:10 -0500

    update

--- compare_interm_w_current.py
+++ compare_interm_w_current.py
@@ -9,6 +9,6 @@ import shared #use shared to call connection from outside of the module
 # shared.my_conn = vamps_upload_util_class.MyConnection('bpcweb7.bpcservers.private', 'vamps2')
 
 if __name__ == '__main__':
-  shared.my_conn = vamps_upload_util_class.MyConnection('bpcweb7.bpcservers.private', 'vamps2')
+  shared.my_conn = vamps_upload_util_class.MyConnection('vampsdb', 'vamps')
   vamps_upload_util_class.SqlUtil().compare_interm_w_current()
 
--- compare_res_w_previous.py
+++ compare_res_w_previous.py
@@ -11,6 +11,7 @@ import shared #use shared to call connection from outside of the module
 # shared.my_conn = vamps_upload_util_class.MyConnection('bpcweb7.bpcservers.private', 'vamps2')
 
 if __name__ == '__main__':
-  shared.my_conn = vamps_upload_util_class.MyConnection('bpcweb7.bpcservers.private', 'vamps2')
-  vamps_upload_util_class.SqlUtil().compare_res_w_previous(table_list = ["vamps_data_cube", "vamps_export", "vamps_junk_data_cube", "vamps_projects_datasets", "vamps_sequences", "vamps_taxonomy"])
+  shared.my_conn = vamps_upload_util_class.MyConnection('vampsdb', 'vamps')
+#  shared.my_conn = vamps_upload_util_class.MyConnection('bpcweb7.bpcservers.private', 'vamps2')
+  vamps_upload_util_class.SqlUtil().compare_res_w_previous(table_list = ["vamps_data_cube", "vamps_junk_data_cube", "vamps_projects_datasets", "vamps_sequences", "vamps_taxonomy"])
 
